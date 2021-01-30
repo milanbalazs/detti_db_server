70f104fdb05037ea8180e3eee99e4f936ceaa152
milanbalazs <milanbalazs01@gmail.com>
2021-01-30 11:50:30 +0100

    Add GET/PUT UnitTests for server

--- test/test_server_ut.py
+++ test/test_server_ut.py
@@ -3,6 +3,8 @@ import sys
 import os
 import warnings
 import configparser
+import subprocess
+import requests
 from typing import Optional
 
 sys.path.append(os.path.join(os.path.realpath(os.path.dirname(__file__)), ".."))
@@ -25,6 +27,8 @@ class DettiServerTestCases(unittest.TestCase):
     This class contains all TestCases for detti DB.
     """
 
+    proc: Optional[subprocess.Popen] = None
+
     def __init__(self, *args, **kwargs) -> None:
         super(DettiServerTestCases, self).__init__(*args, **kwargs)
         # Show the complete diff in case of error
@@ -33,21 +37,34 @@ class DettiServerTestCases(unittest.TestCase):
             os.path.realpath(os.path.dirname(__file__)), "detti_conf_ut.ini"
         )
 
-    def setUp(self) -> None:
+    @classmethod
+    def setUpClass(cls) -> None:
         """
-        Running before all methods.
-        It is an init method for test methods.
-        :return:
+        Running (once) before starting to run the test methods.
+        It is and init method for complete UnitTests
+        :return: None
         """
 
         warnings.filterwarnings("ignore", category=ResourceWarning)
 
-    def tearDown(self) -> None:
+        cls.proc: Optional[subprocess.Popen] = subprocess.Popen(
+            "source venv/bin/activate && python detti_server.py --config_file {}".format(
+                os.path.join(os.path.realpath(os.path.dirname(__file__)), "detti_conf_ut.ini")
+            ).split(),
+            executable="/bin/bash",
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
         """
-        Running after all methods.
-        It is the cleaner method.
+        Running (once) after finishing all test methods.
+        It is and "destructor" method for complete UnitTests
         :return: None
         """
+
+        if cls.proc:
+            cls.proc.terminate()
+            cls.proc.wait()
 
         config: configparser.ConfigParser = configparser.ConfigParser(allow_no_value=True)
         config.read(os.path.join(os.path.realpath(os.path.dirname(__file__)), "detti_conf_ut.ini"))
@@ -55,5 +72,26 @@ class DettiServerTestCases(unittest.TestCase):
         if os.path.isfile(db_file_path):
             os.remove(db_file_path)
 
-    def test_ok(self):
-        self.assertTrue(True)
+    def test_get_not_exist_element(self) -> None:
+        """
+        Testing when the requested element is not in DB.
+        :return: None
+        """
+
+        resp = requests.get("http://localhost:5000/get/not_exist")
+        self.assertEqual(resp.status_code, 201)
+        self.assertTrue("'not_exist' key doesn't exist in DB." in resp.text)
+
+    def test_get_exist_element(self) -> None:
+        """
+        Testing when the requested element is in DB.
+        :return: None
+        """
+
+        put_resp = requests.put("http://localhost:5000/set", data={"exist": "value_of_exist_key"})
+        self.assertEqual(put_resp.status_code, 200)
+        self.assertTrue("OK" in put_resp.text)
+
+        resp = requests.get("http://localhost:5000/get/exist")
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json(), {"exist": "value_of_exist_key"})
