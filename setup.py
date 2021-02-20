534585d4ed668dea3daee03b2e5eb26fd758bca8
milanbalazs <milanbalazs01@gmail.com>
2021-02-20 22:44:31 +0100

    Update setup.py file

--- setup.py
+++ setup.py
@@ -17,22 +17,27 @@ REQ_FILE_PATH = os.path.join(PATH_OF_FILE_DIR, "requirements.txt")
 
 
 def load_requirements(fname):
-    reqs = parse_requirements(fname, session="test")
-    return [str(ir.req) for ir in reqs]
+    reqs = parse_requirements(fname, session=False)
+    try:
+        requirements = [str(ir.req) for ir in reqs]
+    except AttributeError:
+        requirements = [str(ir.requirement) for ir in reqs]
+    return requirements
 
 
-with open("README.md", "r") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="detti_db_server",
     version="0.1.0",
     description="Lightweight Json based key-value DB and/or server.",
+    long_description_content_type="text/markdown",
     long_description=long_description,
     author="Milan Balazs",
     author_email="milanbalazs01@gmail.com",
     license="three-clause BSD",
-    python_requires=">=3.6.0",
+    python_requires=">=3.6",
     url="https://github.com/milanbalazs/detti_db_server",
     install_requires=load_requirements(REQ_FILE_PATH),
     classifiers=[
@@ -40,7 +45,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy"
+        "Programming Language :: Python :: Implementation :: PyPy",
         "License :: OSI Approved :: BSD License",
         "Intended Audience :: Developers",
         "Topic :: Database",
